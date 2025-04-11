f56d4a182fd0eaf924d3e6cb817833e494c67d7f
shu <106082751+shu1411@users.noreply.github.com>
2025-04-12 00:50:11 +0200

    update gameloop to track players in groups

--- main.py
+++ main.py
@@ -6,25 +6,37 @@ def main():
     print("Starting Asteroids!")
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     # spawn player in the middle of the screen
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
+    dt = 0
 
     # start game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        
+        # update each player
+        updatable.update(dt)
+        
+        screen.fill("black")
+
+        # draw each player
+        for p in drawable:
+            p.draw(screen)
+
         pygame.display.flip()
 
         # convert from milliseconds to seconds
         dt = clock.tick(60) / 1000
-
 
 
 if __name__ == "__main__":
