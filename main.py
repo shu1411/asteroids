284809d468d988d7493e6f0e385de9577d2da2e5
shu <106082751+shu1411@users.noreply.github.com>
2025-04-11 23:54:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # start game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
