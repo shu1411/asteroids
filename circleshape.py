286e578383ea3a0f8062e5685697405944120bec
shu <106082751+shu1411@users.noreply.github.com>
2025-04-12 00:28:16 +0200

    add circleshape.py and player.py
    create classes to draw the player to the screen

---
+++ circleshape.py
@@ -0,0 +1,23 @@
+import pygame
+
+# base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+        
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    
+    def draw(self, screen):
+        # child classes must override
+        pass
+
+    def update(self, dt):
+        # child classes must override
+        pass
--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    pygame.init()
     print("Starting Asteroids!")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    # spawn player in the middle of the screen
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
     # start game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+        # convert from milliseconds to seconds
+        dt = clock.tick(60) / 1000
 
 
 
